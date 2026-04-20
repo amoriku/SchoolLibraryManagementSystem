88ef86eee59647d3c7e8ab2a1c035c8ac90044e3
Amoriku <markshangini@gmail.com>
2026-04-20 19:35:01 +0300

    Update Infrastructure

---
+++ SchoolLibrary.Infrastructure/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SchoolLibrary.Domain.Entities;
+
+namespace SchoolLibrary.Infrastructure
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; } 
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
