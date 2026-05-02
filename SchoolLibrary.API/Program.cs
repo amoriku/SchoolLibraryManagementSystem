1ba87261647005e0e51308f6c1a07028de79ab35
Amoriku <markshangini@gmail.com>
2026-05-02 10:19:37 +0300

    Update Program.cs

--- SchoolLibrary.API/Program.cs
+++ SchoolLibrary.API/Program.cs
@@ -1,10 +1,26 @@
+using Microsoft.AspNetCore.Identity;
+using SchoolLibrary.Application;
+using SchoolLibrary.Domain.Entities;
+using SchoolLibrary.Infrastructure;
+using SchoolLibrary.Infrastructure.Common;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services
+    .AddIdentityCore<ApplicationUser>(options => { })
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddLogging();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -12,6 +28,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -20,4 +38,14 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+DataSeeding();
 app.Run();
+
+void DataSeeding()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
